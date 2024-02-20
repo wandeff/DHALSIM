dcbb789cb8026087f28bb0a451cae35195c2aae6
truthtang <2973086644@qq.com>
2024-02-20 22:19:19 +0800

    contract

--- dhalsim/python2/smartcontract.py
+++ dhalsim/python2/smartcontract.py
@@ -1,6 +1,9 @@
+# coding=utf-8
 import json
 import os
-import pickle
+import time
+
+import jsonpickle
 from entities.control import AboveControl, BelowControl, TimeControl
 
 
@@ -11,40 +14,38 @@ class smartContract:
         """
         self.blockchain_path = None
         self.index = None
-        self.submit(pickle.dumps(controls))
-        self.plc = plc
+        self.path = os.path.join(plc.intermediate_yaml['output_path'], plc.intermediate_plc['name'])
+        self.submit(jsonpickle.encode(controls))
 
     def submit(self, controls):
         """
         写入区块
         """
-        untx_path = os.path.join(self.plc.intermediate_yaml['output_path'], self.plc.intermediate_plc['name'], 'untx')
+        untx_path = os.path.join(self.path, 'untx')
 
-        sign_path = os.path.join(self.plc.intermediate_yaml['output_path'], self.plc.intermediate_plc['name'], 'sign')
+        sign_path = os.path.join(self.path, 'sign')
 
         while not self.get_sign(sign_path, 1):
             pass
 
-        json_data = json.dumps(controls, indent=2)
+        json_data = json.dumps(controls)
 
-        with open(untx_path, 'w') as f:
+        with open(untx_path, 'wb') as f:
             f.write(json_data)
 
-        self.set_sign(sign_path, 0)
-
-        self.blockchain_path = os.path.join(self.plc.intermediate_yaml['output_path'], self.plc.intermediate_plc['name'], 'blockchain')
-
-        with open(self.blockchain_path, 'r+') as f:
-            raw = f.readline()
-        if len(raw) > 0:
-            data = json.loads(raw)
-        else:
-            data = []
-
-        for item in data:
-            if item['tx'] == json_data:
-                self.index = item['index']
-                break
+        self.blockchain_path = os.path.join(self.path, 'blockchain')
+        while 1:
+            time.sleep(0.3)
+            with open(self.blockchain_path, 'rb') as f:
+                raw = f.readline()
+            if len(raw) > 0:
+                data = json.loads(raw)
+            else:
+                data = []
+            for item in data:
+                if item['tx'] == json_data:
+                    self.index = item['index']
+                    break
 
 
     def checkrun(self, plc, local_controls):
@@ -52,7 +53,7 @@ class smartContract:
         check controls
         """
         # 从区块链中找对应数据
-        with open(self.blockchain_path, 'r+') as f:
+        with open(self.blockchain_path, 'rb') as f:
             raw = f.readline()
         if len(raw) > 0:
             data = json.loads(raw)
@@ -62,11 +63,13 @@ class smartContract:
         for item in data:
             if item['index'] == self.index:
                 controls = item['tx']
-                if pickle.dumps(local_controls) == controls:
+                if json.dumps(jsonpickle.encode(local_controls)) == controls:
                     for control in local_controls:
                         control.apply(plc)
+                else:
+                    for control in jsonpickle.decode(json.loads(controls)):
+                        control.apply(plc)
                 break
-        pass
 
     @staticmethod
     def get_sign(sign_path, expected_value):
