d76046155054f55c1e7c56d8f4cb03df08fbfa5c
wandeff <2438178114@qq.com>
2025-03-04 13:13:03 +0800

    delete webgui

--- dhalsim/command_line.py
+++ dhalsim/command_line.py
@@ -71,7 +71,7 @@ class Runner():
         self.automatic_run = subprocess.Popen(
             ["python2", str(automatic_run_path), str(intermediate_yaml_path)])
 
-        subprocess.call(["webgui", str(intermediate_yaml_path)])
+        # subprocess.call(["webgui", str(intermediate_yaml_path)])
         self.automatic_run.wait()
 
 
