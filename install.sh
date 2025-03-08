9a45658cd4691ad46e544dd2a35b868741cfc684
wandeff <2438178114@qq.com>
2025-03-08 21:50:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,12 +37,6 @@ sudo apt update
 # Installing necessary packages
 sudo apt install -y python3 python3-pip 
 
-# WEBGUI
-#cd ~
-#git clone --depth 1 https://github.com/WandeF/WebGUI.git
-#cd WebGUI
-#./install.sh
-
 
 
 sudo apt install -y python2
